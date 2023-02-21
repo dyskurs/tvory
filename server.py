0f70f026799b50f2708f5822742b8e5ca219389b
dyskurs <52207169+dyskurs@users.noreply.github.com>
2023-02-21 01:42:37 +0100

    add

--- server.py
+++ server.py
@@ -7,6 +7,8 @@ import yaml
 from sys import argv
 from pathlib import Path
 import os
+import glob
+from datetime import date
 
 
 # Open the file and load the file
@@ -18,23 +20,44 @@ hostName = "localhost"
 serverPort = 8080
 
 df = pd.read_csv("central.csv", keep_default_na = False)
-static = pd.read_csv("static.csv")
+try:
+    static = pd.read_csv("static.csv")
+except:
+    static = None
 endpoints = set(df["endpoint"])
-static_endpoints = set(static["endpoint"])
+static_endpoints = set(static["endpoint"]) if static is not None else set([])
 templates = {}
 templateLoader = jj.FileSystemLoader(searchpath="_layouts")
 templateEnv = jj.Environment(loader=templateLoader)
+templateEnv.globals["now"] = date.today()
+templateEnv.globals.update(zip=zip)
+
 for temp_name in set(df["template"]):
     templates[temp_name] = templateEnv.get_template(temp_name + ".html")
 
 
 pages = []
 for _, page in df.iterrows():
-    with open(page["folder"] + page["filename"], "r") as fin:
-        page["content"] = fin.read()
-        pages += [dict(page)]
+    try:
+        with open(page["dir"] + page["filename"], "r") as fin:
+            page["content"] = fin.read()
+    except:
+        page["content"] = ""
+    pages += [dict(page)]
 sitedict["pages"] = pages
 #del pages
+
+data = {}
+for filename in glob.glob("_data/*"):
+    file_ext = filename.split(".")[-1]
+    proper_filename = filename.split("/")[-1][:-len(file_ext)-1]
+    if file_ext == "csv":
+        data[proper_filename] = pd.read_csv(filename, keep_default_na=False).to_dict('records')
+    elif file_ext == "yml":
+        with open(filename, "r") as fin:
+            data[proper_filename] = yaml.safe_load(fin)   
+
+sitedict["data"] = data
 
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -53,9 +76,12 @@ class MyServer(BaseHTTPRequestHandler):
             
             page = df[df["endpoint"]==self.path].iloc[0]
             
-            with open(page["folder"] + page["filename"], "r") as fin:
-                content = fin.read()
-            
+            try:
+                with open(page["dir"] + page["filename"], "r") as fin:
+                    content = fin.read()
+            except:
+                content = ""
+                
             template = templateEnv.get_template(page["template"] + ".html")
             page["content"] = md.markdown(content)
             self.wfile.write(bytes(template.render({"page": page, "site": sitedict}), "utf-8"))
@@ -76,7 +102,7 @@ class MyServer(BaseHTTPRequestHandler):
                 self.send_header('Content-type', 'text/html')
             self.end_headers()
             info = static[static["endpoint"]==self.path].iloc[0]
-            with open(info["folder"] + info["filename"], "rb") as fin:
+            with open(info["dir"] + info["filename"], "rb") as fin:
                 self.wfile.write(fin.read())
             
         else:
@@ -85,22 +111,25 @@ class MyServer(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(bytes(f'<html><head><meta http-equiv="refresh" content="0; URL=/404"></head><body></body></html>', "utf-8"))
 
-def treat_central(page):
+def treat_central(page, output_folder = "_site"):
     if page["endpoint"].endswith("/"):
         page["endpoint"] += "index.html"
-    with open(page["folder"] + page["filename"], "r") as fin:
-        content = fin.read()
+    try:
+        with open(page["dir"] + page["filename"], "r") as fin:
+            content = fin.read()
+    except:
+        content = ""
     template = templateEnv.get_template(page["template"] + ".html")
     page["content"] = md.markdown(content)
-    filename = "_site" + page["endpoint"]
+    filename = output_folder + page["endpoint"]
     Path(os.path.dirname(filename)).mkdir(parents=True, exist_ok=True)
     with open(filename, "w+") as fout:
         fout.write(template.render({"page": page, "site": sitedict}))
 
 
-def treat_static(info):
-    with open(info["folder"] + info["filename"], "rb") as fin:
-        filename = "_site" + info["endpoint"]
+def treat_static(info, output_folder = "_site"):
+    with open(info["dir"] + info["filename"], "rb") as fin:
+        filename = output_folder + info["endpoint"]
         Path(os.path.dirname(filename)).mkdir(parents=True, exist_ok=True)
         with open(filename, "wb+") as fout:
             fout.write(fin.read())
@@ -108,8 +137,12 @@ def treat_static(info):
 
 if __name__ == "__main__":
     if len(argv) > 1 and argv[1]=="build":
-        df.apply(treat_central, axis = 1)
-        static.apply(treat_static, axis = 1)
+        output_folder = "_site"
+        if len(argv) > 2:
+            output_folder = argv[2]
+        df.apply(treat_central, output_folder = output_folder, axis = 1)
+        if static is not None:
+            static.apply(treat_static, output_folder = output_folder, axis = 1)
     else:
         webServer = HTTPServer((hostName, serverPort), MyServer)
         print(f"Server started http://{hostName}:{serverPort}")
@@ -121,7 +154,3 @@ if __name__ == "__main__":
 
         webServer.server_close()
         print("Server stopped.")
-
-
-
-
